2349155694400d25fb6f65c7d41825b3e3fe37e8
maseretikevin <92727539+maseretikevin@users.noreply.github.com>
2023-11-21 21:27:09 +0300

    Added Annotation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { Canvas } from "@react-three/fiber";
 import { House } from "../src/assets/Roomportfolio";
 import { Me } from "../src/assets/Mycharacter";
+import { extend } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+import { geometry } from "maath";
+
+extend(geometry);
 import {
   CameraControls,
   PerspectiveCamera,
@@ -18,10 +23,34 @@ function App() {
         <group scale={15} position={[32, 5, 17]} rotation={[-1.5, 0, 0]}>
           <Me />
         </group>
+        <group scale={4} position={[4, 1, 28]}>
+          <Annotation>
+            Click Me <span style={{ fontSize: "1.5em" }}>🌖</span>
+          </Annotation>
+        </group>
       </group>
-      <Environment preset="city" background blur={1} />
-      <PerspectiveCamera makeDefault position={[0, 0, 18.5]} />
+
+      <PerspectiveCamera makeDefault position={[30, 18, 788.5]} />
     </Canvas>
+  );
+}
+function Annotation({ children, ...props }) {
+  return (
+    <Html
+      {...props}
+      transform
+      // occlude="blending"
+      geometry={
+        /** The geometry is optional, it allows you to use any shape.
+         *  By default it would be a plane. We need round edges here ...
+         */
+        <roundedPlaneGeometry args={[1.66, 0.47, 0.24]} />
+      }
+    >
+      <div className="annotation" onClick={() => console.log(".")}>
+        {children}
+      </div>
+    </Html>
   );
 }
 
