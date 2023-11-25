6f9efe60f3b551e9997acd85ede59be70b17cc27
maseretikevin <92727539+maseretikevin@users.noreply.github.com>
2023-11-25 21:05:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function Models() {
 
 function App() {
   return (
-    <Suspense fallback={<div style={{position: "absolute", marginTop: "20px", alignItems:"center"}}><Loader /></div>}>
+    <Suspense fallback={<div style={{position: "absolute", marginTop: "50px", marginLeft: "70px", alignItems:"center"}}><Loader /></div>}>
     <Models/>
     </Suspense>
   );
